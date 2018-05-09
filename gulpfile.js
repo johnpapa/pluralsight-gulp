e3d3b3cf54b0d192f22364c66acfe9cbc50545e0
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-09 18:43:57 +0900

    Modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
-const gulp = require('gulp');
-const jshint = require('gulp-jshint');
-const jscs = require('gulp-jscs');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var print = require('gulp-print').default;
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
 gulp.task('vet', function() {
+  log('Analyzing source with JSHint and JSCS');
+
   return gulp
     .src([
       './src/**/*.js',
       './*.js'
     ])
+    .pipe(gulpif(args.verbose, print())) // NOTE: print when --verbose is passed
     .pipe(jscs())
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+    .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
+    .pipe(jshint.reporter('fail'));
 });
+
+function log(msg) {
+  if (typeof(msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        util.log(util.colors.blue(msg[item]));
+      } else {
+        util.log(util.colors.blue(msg));
+      }
+    }
+  }
+}
